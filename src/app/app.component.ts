cb7fabd55a55ceb2c2a7a2bcaa5abf704b9c8b1d
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2023-01-02 15:32:10 +0100

    add error handler and http interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ErrorHandler } from '@angular/core';
+import { HttpServiceService } from './servizi/http-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NotifierService } from './servizi/notifier.service';
+import { HttpResponse, HttpResponseBase } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'http-request';
+  loadedProducts: any = [];
+  productForm!: FormGroup;
+  myErrorMessage: any;
+
+  constructor(
+    private httpService: HttpServiceService,
+    private formBuilder: FormBuilder,
+    private notifierService: NotifierService
+  ) {}
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      titolo: ['', Validators.required],
+      prezzo: ['', Validators.required],
+    });
+  }
+
+  onGetProducts() {
+    this.httpService.getUrl().subscribe({
+      next: (response) => {
+        this.notifierService.showNotification(
+          'Richiesta effettuata con successo!',
+          'ok',
+          'success'
+        );
+        console.log(response);
+      },
+      error: (error) => {
+        this.myErrorMessage = error;
+      },
+    });
+  }
+
+  onCreateProduct(productData: { titolo: string; prezzo: string }) {
+    this.httpService.postUrl(productData).subscribe({
+      next: (response) => {
+        this.loadedProducts = response;
+        this.notifierService.showNotification(
+          'Prodotto aggiunto con successo',
+          'ok',
+          'success'
+        );
+        this.productForm.reset();
+      },
+      error: (error) => {
+        this.myErrorMessage = error;
+      },
+    });
+  }
+
+  onDelete(productData: { id: any }) {
+    this.httpService.deleteUrl(productData.id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './servizi/auth-interceptor.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { NotifierComponent } from './notifier/notifier.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NotifierComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
